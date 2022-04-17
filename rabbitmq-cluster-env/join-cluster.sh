819d221c0662687eb5366071c5ec89e5cb93d9b5
chun-huang <hgfrttlk@gmail.com>
2022-04-17 21:49:38 +0800

    Add .gitignore and add annotation to join-cluster.sh

--- rabbitmq-cluster-env/join-cluster.sh
+++ rabbitmq-cluster-env/join-cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 在背景啟動 rabbitmq service
+# 在背景啟動 rabbitmq service (erlang node and rabbitmq application , detached mean in background)
 /usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
 # 等待 rabbitmq 啟動完畢
 sleep 5s
@@ -9,7 +9,7 @@ rabbitmqctl stop_app
 rabbitmqctl join_cluster rabbit@rabbitmq-cluster-01
 # 啟動 rabbitmq application 來 sync 資料
 rabbitmqctl start_app
-# 停止 rabbitmq service
+# 停止 rabbitmq service (errlang node and rabbitmq application )
 rabbitmqctl stop
 # 等待 rabbitmq service 完全停止
 sleep 2s
