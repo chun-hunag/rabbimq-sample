45d3b9c1c50829c63a736e5b8ab7032af222e7fa
chun-huang <hgfrttlk@gmail.com>
2022-05-08 14:40:25 +0800

    Update cluster config setting

--- rabbitmq-cluster-env/rabbitmq/join-cluster.sh
+++ rabbitmq-cluster-env/rabbitmq/join-cluster.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# 在背景啟動 rabbitmq service (erlang node and rabbitmq application , detached mean in background)
+# start rabbitmq service in backgorund (erlang node and rabbitmq application , detached mean in background)
 rabbitmq-server -detached
 sleep 5s
 
-#停止 rabbitmq application
+# stop rabbitmq application
 rabbitmqctl stop_app
 
-# 加入 rabbimtq cluster
+# join rabbimtq cluster
 rabbitmqctl join_cluster ${MASTER_NODE_HOST_NAME}
 
-# 啟動 rabbitmq application 來 sync 資料
+# start rabbitmq application 來 sync 資料
 rabbitmqctl start_app
 
-# 停止 rabbitmq service (errlang node and rabbitmq application )
+# stop rabbitmq service (errlang node and rabbitmq application )
 rabbitmqctl stop
 sleep 5s
 
-# 將 rabbitmq service 啟動在前景
+# start rabbitmq service foreground
 rabbitmq-server
